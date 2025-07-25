dc27f6c3760acac89fa2f4ccd83e02c7335bd9f0
Muhammad Usman Khan <usmanjoiya981@gmail.com>
2025-07-25 00:02:20 -0700

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://society-managment-system-backend.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
